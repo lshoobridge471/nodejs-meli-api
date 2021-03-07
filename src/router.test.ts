0a0ced9702696482b53beb81c86747b962ce45f6
Lucas Shoobridge <shoobridgelucas@hotmail.com>
2021-03-07 12:51:46 -0300

    Add routes UT.

--- src/router.test.ts
+++ src/router.test.ts
@@ -1,11 +1,17 @@
-import { Express, Router } from 'express';
+import { Router } from 'express';
 import router from './router';
-import { createExpressApp } from './utils/utils';
+import routes from './routes/routes';
+import { AddRouteProps } from './types/interfaces';
 
 describe('router test suite', () => {
-    it('test routes created', () => {
+    it('test routes created successfully', () => {
         const customRouter: Router = router;
-        const app: Express = createExpressApp(customRouter);
-        console.log(customRouter.stack);
+
+        const routesNames = routes.map((route: Omit<AddRouteProps, 'router'>) => route.handler.name);
+        const appRoutesNames = customRouter.stack.map((route: any) => route.name);
+        
+        expect(routesNames).toEqual(appRoutesNames);
+        expect(routesNames).toHaveLength(routes.length);
+        expect(appRoutesNames).toHaveLength(routes.length);
     });
 });
