5f8cea73e0679a8c857ebaefd64a8939fdc0e15c
Lucas Shoobridge <shoobridgelucas@hotmail.com>
2021-03-09 15:06:44 -0300

    Add cors + Morgan logger.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import express, { json } from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 import router from './router'
 import CONFIG from './config/config';
 import { toInteger } from './utils/utils';
@@ -6,14 +8,22 @@ import { toInteger } from './utils/utils';
 // Obtain the environments variables or the default values.
 const {
     SERVER_HOST,
-    SERVER_PORT
+    SERVER_PORT,
+    NODE_ENV
 } = CONFIG;
+const isDevelopment = NODE_ENV == 'development';
 
 // Initialize the Express APP.
 const app = express();
 
+// Enable morgan for logging requests.
+if(isDevelopment) {
+    // Only add in development
+    app.use(morgan('dev'));
+}
 app.use('/', router) // Mount the router on the Express App.
 app.use(json) // Use the JSON wrapper.
+app.use(cors()) // Enable cors.
 .listen(toInteger(SERVER_PORT), SERVER_HOST, () => { // Start the Express server
     //eslint-disable-next-line
     console.log(`NodeJS Express server started at http://${SERVER_HOST}:${SERVER_PORT}` );
